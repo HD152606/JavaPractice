e212bb69528bd42fbf13ed94ea79c2648e8e3f8c
Kimminhyeong <Kimminhyeong@localhost>
2017-03-27 12:08:57 +0900

    Car2

---
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *     |_Car2
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 24.
+ * </pre>
+ * @author      : Kimminhyeong
+ * @version     : 1.0
+ */
+public class Car2 {
+	private String company="현대자동차";
+	private String model="제네시스";
+	private String color="검정색";
+	private int maxSpeed=225;
+	private int price=50000000;
+	
+	public Car2(){
+	}
+	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice){
+		company = newCompany;
+		model = newModel;
+		color = newColor;
+		maxSpeed = newMaxSpeed;
+		price = newPrice;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public int getPrice() {
+		return price;
+	}
+
+}
