de0cf4ed051babedc11b8172cc7a853f33a56108
Kimminhyeong <Kimminhyeong@localhost>
2017-04-17 09:15:25 +0900

    Test

---
+++ JavaPractice/src/org/dimigo/oop/Test.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_Test
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 17.
+ * </pre>
+ *
+ * @author    : Kimminhyeong
+ * @version   : 1.0
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
