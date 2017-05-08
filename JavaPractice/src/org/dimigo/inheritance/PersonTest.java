b6993e8b6bf266710f95198491a86e69d0d0ad9b
Kimminhyeong <Kimminhyeong@localhost>
2017-05-08 12:52:00 +0900

    Person

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    |_PersonTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ *
+ * @author    : Kimminhyeong
+ * @version   : 1.0
+ */
+public class PersonTest {
+
+	
+	public static void main(String[] args) {
+		Person p = new Person("Tom");
+		System.out.println(p);
+		
+		Korean k = new Korean("홍길동");
+		Japanese j = new Japanese("다나카");
+		Chinese c = new Chinese("왕밍");
+		System.out.println(k);
+		System.out.println(j);
+		System.out.println(c);
+		System.out.println();
+		p.sayHello();
+		k.sayHello();
+		j.sayHello();
+		c.sayHello();
+		System.out.println();
+		p.sayBye();
+		k.sayBye();
+		j.sayBye();
+		c.sayBye();
+
+	}
+
+}
